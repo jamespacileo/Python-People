0d74e72da9425422d02c1ca82876fc746b150ceb
Dan Stephenson <dan@ibox.net>
2012-03-13 00:41:16 -0700

    blah

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class User(models.Model):
+class PythonPerson(models.Model):
 	first_name = models.CharField('First name', max_length=100)
 	last_name = models.CharField('Last name', max_length=100)
 	email = models.EmailField('Email', max_length=40, null=True)
